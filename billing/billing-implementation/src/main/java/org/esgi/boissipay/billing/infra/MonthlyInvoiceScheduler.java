7c5a7a2e8ab7d30bae072c16b20a3191b92f7a65
Swann HERRERA <swann@devloup.dev>
2022-07-13 16:37:50 +0200

    rename and add todo

--- billing/billing-implementation/src/main/java/org/esgi/boissipay/billing/infra/MonthlyInvoiceScheduler.java
+++ billing/billing-implementation/src/main/java/org/esgi/boissipay/billing/infra/MonthlyInvoiceScheduler.java
@@ -5,10 +5,10 @@ import java.time.ZonedDateTime;
 import org.esgi.boissipay.billing.domain.BillingRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 
-public class MonthlyPaymentScheduler {
+public final class MonthlyInvoiceScheduler {
     private final BillingRepository billingRepository;
 
-    public MonthlyPaymentScheduler(BillingRepository billingRepository) {
+    public MonthlyInvoiceScheduler(BillingRepository billingRepository) {
         this.billingRepository = billingRepository;
     }
 
@@ -16,5 +16,13 @@ public class MonthlyPaymentScheduler {
     public void createInvoice() {
         var bills = billingRepository.getBillOfLastMonth(ZonedDateTime.now());
         // TODO for each bills notify user concerned
+
+        // faire emergée une notion de payment et de bill
+        // scheduler quand il faut
+        // mettre les schemas des events a jour.
+        // reception des events côte contract
+        // mise en place d'une "clean archi côté contrat ?"
+        // mise en place de redis (plus côté contrat)
+        // notifier les utilisateurs
     }
 }
