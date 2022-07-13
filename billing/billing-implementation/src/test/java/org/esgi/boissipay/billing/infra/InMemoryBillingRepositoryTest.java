f838242bb466ead3b8e371e29655800d6fe746e2
Swann HERRERA <swann@devloup.dev>
2022-07-13 16:07:25 +0200

    test use case not consumer

--- billing/billing-implementation/src/test/java/org/esgi/boissipay/billing/infra/InMemoryBillingRepositoryTest.java
+++
@@ -1,4 +0,0 @@
-package org.esgi.boissipay.billing.infra;
-
-public class InMemoryBillingRepositoryTest {
-}
--- billing/billing-implementation/src/test/java/org/esgi/boissipay/billing/use_case/CreateBillingUseCaseTest.java
+++ billing/billing-implementation/src/test/java/org/esgi/boissipay/billing/use_case/CreateBillingUseCaseTest.java
@@ -1,10 +1,10 @@
-package org.esgi.boissipay.billing.kafka;
+package org.esgi.boissipay.billing.use_case;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.esgi.boissipay.billing.infra.EventDispatcher;
 import org.esgi.boissipay.billing.infra.InMemoryBillingRepository;
-import org.esgi.boissipay.billing.use_case.CreateBillingUseCase;
+import org.esgi.boissipay.billing.kafka.Consumer;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +12,11 @@ import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public final class ConsumerTest {
+public class CreateBillingUseCaseTest {
 
     private final InMemoryBillingRepository repo = InMemoryBillingRepository.newEmptyInMemoryBillingRepository();
     private final static ObjectMapper mapper = new ObjectMapper();
     private final CreateBillingUseCase createBillingUseCase = new CreateBillingUseCase(repo, mapper, new EventDispatcher(Collections.emptySet()));
-    private final Consumer consumer = new Consumer(createBillingUseCase);
 
     @BeforeAll
     static void beforeAll() {
@@ -25,9 +24,9 @@ public final class ConsumerTest {
     }
 
     @Test
-    void test_consume() {
+    void test_create_billing() {
         var kafkaInput = "{\"name\": \"test\"}";
-        consumer.consume(kafkaInput);
+        createBillingUseCase.createBilling(kafkaInput);
         assertEquals(repo.count(), 1);
     }
 }
