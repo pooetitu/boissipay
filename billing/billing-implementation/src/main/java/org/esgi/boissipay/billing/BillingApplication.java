a6cd89bff7f198da9ba6f7dce16f3490e2ca8b3d
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-07-16 18:32:33 +0200

    update scheduler

--- billing/billing-implementation/src/main/java/org/esgi/boissipay/billing/BillingApplication.java
+++ billing/billing-implementation/src/main/java/org/esgi/boissipay/billing/BillingApplication.java
@@ -2,8 +2,10 @@ package org.esgi.boissipay.billing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BillingApplication {
 
     public static void main(String[] args) {
--- billing/billing-implementation/src/main/java/org/esgi/boissipay/billing/infra/MonthlyBillingScheduler.java
+++ billing/billing-implementation/src/main/java/org/esgi/boissipay/billing/infra/MonthlyBillingScheduler.java
@@ -1,25 +1,28 @@
 package org.esgi.boissipay.billing.infra;
 
+import org.esgi.boissipay.billing.domain.event.EventDispatcher;
 import org.esgi.boissipay.billing.domain.models.Invoice;
 import org.esgi.boissipay.billing.domain.repository.ContractRepository;
 import org.esgi.boissipay.billing.domain.repository.PaymentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
+@Service
 public final class MonthlyBillingScheduler {
     private static final Logger logger = LoggerFactory.getLogger(MonthlyBillingScheduler.class);
     private final PaymentRepository paymentRepository;
 
     private final ContractRepository contractRepository;
-    private final DefaultEventDispatcher eventDispatcher;
+    private final EventDispatcher eventDispatcher;
 
-    public MonthlyBillingScheduler(PaymentRepository paymentRepository, ContractRepository contractRepository, DefaultEventDispatcher eventDispatcher) {
+    public MonthlyBillingScheduler(PaymentRepository paymentRepository, ContractRepository contractRepository, EventDispatcher eventDispatcher) {
         this.paymentRepository = paymentRepository;
         this.contractRepository = contractRepository;
         this.eventDispatcher = eventDispatcher;
