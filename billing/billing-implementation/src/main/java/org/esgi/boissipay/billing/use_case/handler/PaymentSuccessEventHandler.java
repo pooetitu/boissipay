a9bb7e43ddf7137c278952c1e0c30f50982ad754
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-07-22 17:07:44 +0200

    Add log to inspect payment success event handler issue

--- billing/billing-implementation/src/main/java/org/esgi/boissipay/billing/use_case/handler/PaymentSuccessEventHandler.java
+++ billing/billing-implementation/src/main/java/org/esgi/boissipay/billing/use_case/handler/PaymentSuccessEventHandler.java
@@ -3,9 +3,13 @@ package org.esgi.boissipay.billing.use_case.handler;
 import org.esgi.boissipay.billing.domain.event.PaymentSuccessHandler;
 import org.esgi.boissipay.billing.domain.models.Payment;
 import org.esgi.boissipay.billing.use_case.ValidatePaymentUseCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PaymentSuccessEventHandler implements PaymentSuccessHandler {
     private final ValidatePaymentUseCase validatePaymentUseCase;
+
+    private static final Logger logger = LoggerFactory.getLogger(PaymentSuccessEventHandler.class);
 
     public PaymentSuccessEventHandler(ValidatePaymentUseCase validatePaymentUseCase) {
         this.validatePaymentUseCase = validatePaymentUseCase;
@@ -13,6 +17,7 @@ public class PaymentSuccessEventHandler implements PaymentSuccessHandler {
 
     @Override
     public void onPaymentSuccess(Payment payment) {
+        logger.info("Payment success event handler: " + payment);
         validatePaymentUseCase.validatePayment(payment.id());
     }
 }
